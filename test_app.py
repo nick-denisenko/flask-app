acbc8473de7e7b9621c0e6a6b835db1e505a85cc
Nikolay Denisenko <nikolay.denisenko@quantori.com>
2022-02-11 13:19:01 +0300

    Fixed test

--- test_app.py
+++ test_app.py
@@ -9,4 +9,4 @@ def test_get_hello():
 def test_get_new():
     with app.test_client() as test_client:
         response = test_client.get('/new')
-        assert response.status_code == 200
+        assert response.status_code == 404
