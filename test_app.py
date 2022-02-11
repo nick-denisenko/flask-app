3071ac871fc1f8e875baea785e1bb6d4409e3e72
Nikolay Denisenko <nikolay.denisenko@quantori.com>
2022-02-11 13:14:47 +0300

    Added new test

--- test_app.py
+++ test_app.py
@@ -1,6 +1,12 @@
 from app import app
 
-def test_hello():
+def test_get_hello():
     with app.test_client() as test_client:
         response = test_client.get('/')
+        assert response.status_code == 200
+
+
+def test_get_new():
+    with app.test_client() as test_client:
+        response = test_client.get('/new')
         assert response.status_code == 200
